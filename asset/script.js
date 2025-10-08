5d65586d9fa944c4809bb88f24f7283411096a90
Riqq76 <autolose366@gmail.com>
2025-10-08 17:34:17 +0700

    Add files via upload

---
+++ asset/script.js
@@ -0,0 +1,16 @@
+ // Konfigurasi warna kustom
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            'coffee-dark': '#4b3832',
+            'coffee-light': '#a38b7e',
+            'coffee-accent': '#ff9f1c',
+            'coffee-bg': '#f5f5f5',
+          },
+          fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+          },
+        },
+      },
+    };
